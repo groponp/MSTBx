8b71e4f7ab51ca022770124a2ac661f7c12ced20
Ropón-Palacios G <georcki.ropon@unesp.br>
2026-06-01 15:59:58 -0300

    chore: remove old.code from repository and add to .gitignore

--- old.code/tleap/6n6o/prepLIG.tcl
+++
@@ -1,7 +0,0 @@
-mol new 6N6O.pdb
-set lig [atomselect top "resname KE7"] 
-$lig set resname LIG
-$lig writepdb LIG.pdb
-exec obabel LIG.pdb -O LIGX.mol2 -p 7 
-exec sed -i "s/UNL/LIG/g" LIGX.mol2
-quit 
